23185470c881a034f7d75e383e625534026108b9
rasel <mohammadrussel000@gmail.com>
2022-12-20 16:02:31 +0600

    khj

--- components/head.php
+++ components/head.php
@@ -1,11 +1,14 @@
+<?php
+error_reporting(0);
+?>
 <link rel="stylesheet" href="css/style.css">
 <link rel="shortcut icon" type = "image" href="fav/2.ico">
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-<script src="../js/jquery-3.6.1.min.js"></script>
+<!-- <script src="../js/jquery-3.6.1.min.js"></script> -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script type="text/javascript" src="../js/signature.js"></script>
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-<link href="https://fonts.cdnfonts.com/css/gagalin" rel="stylesheet">
-<link href="//db.onlinewebfonts.com/c/240a7cb10b49b02c94ceddc459d385a9?family=Gagalin" rel="stylesheet" type="text/css"/>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- <script type="text/javascript" src="../js/signature.js"></script> -->
+<!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" /> -->
+<!-- <link href="https://fonts.cdnfonts.com/css/gagalin" rel="stylesheet"> -->
+<!-- <link href="//db.onlinewebfonts.com/c/240a7cb10b49b02c94ceddc459d385a9?family=Gagalin" rel="stylesheet" type="text/css"/> -->
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
--- php/config.php
+++ php/config.php
@@ -1,3 +1,7 @@
 <?php
-$con = mysqli_connect("localhost","root","","dokan");
+$con = mysqli_connect("localhost", "root", "", "dokan");
+?>
+
+<?php
+// $con = mysqli_connect("localhost", "u811705500_happybd", "^[Tupn!6rC", "u811705500_dokan");
 ?>
